cf3011087071e7e5544fed0432cca282f4140cdd
S <s>
2017-06-07 16:18:03 +0100

    {"types":"C","runs":31}

--- Breakout.java
+++ Breakout.java
@@ -61,6 +61,25 @@ public class Breakout extends GraphicsProgram {
 /** Runs the Breakout program. */
 	public void run() {
 		/* You fill this in, along with any subsidiary methods */
+		double x = getWidth() - BRICK_WIDTH;
+ 		double y = getHeight() - BRICK_HEIGHT;
+ 		for (int row = 0; row < 10; row++){
+ 			createRow(x, y, (APPLICATION_WIDTH - row));
+ 			y -= BRICK_HEIGHT;
+ 			x += BRICK_WIDTH;
+ 		}
 	}
+ 		
+ 		private void createRow(double x, double y, int bricks) {
+ 	 		for (int i = 0; i < bricks; i++) {
+ 	 			createBrick((x + i * BRICK_WIDTH), y);
+ 			}
+ 		}
+ 		private void createBrick(double x, double y) {
+ 	 		GRect rect = new GRect(x, y, BRICK_WIDTH, BRICK_HEIGHT);
+ 	 		add(rect);
+ 		
+	}
+
 
 }
