22df1d81118e5708706c3cef3a0057352f34f4d0
S <s>
2017-06-10 10:28:06 +0100

    {"types":"C","runs":7}

--- Breakout.java
+++ Breakout.java
@@ -22,8 +22,8 @@ public class Breakout extends GraphicsProgram {
 	public static final int APPLICATION_HEIGHT = 620;
 
 /** Dimensions of game board (usually the same) */
-	private static final int WIDTH = APPLICATION_WIDTH - 20;
-	private static final int HEIGHT = APPLICATION_HEIGHT - 20;
+	private static final int WIDTH = APPLICATION_WIDTH;
+	private static final int HEIGHT = APPLICATION_HEIGHT;
 
 /** Dimensions of the paddle */
 	private static final int PADDLE_WIDTH = 60;
