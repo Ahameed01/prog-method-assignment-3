f0712b2e09d7a619004fe20be22dde3c70390304
S <s>
2017-06-10 18:22:30 +0100

    {"types":"C","runs":34}

--- Breakout.java
+++ Breakout.java
@@ -114,7 +114,7 @@ public class Breakout extends GraphicsProgram {
  	 private void drawBall(){
  		 GOval ball = new GOval (WIDTH/2 - BALL_RADIUS, HEIGHT/2 - BALL_RADIUS, BALL_RADIUS, BALL_RADIUS);
  		 ball.setFilled(true);
- 		 ball.setColor(Color.WHITE);
+ 		 ball.setColor(Color.PINK);
  		 add (ball);
  	 }
  		
